557caaa18b2a2527e6e937cef09a7d2de1c3efec
aarthigopinath19 <aarthiravi19@gmail.com>
2022-11-06 09:06:00 +0530

    project structure

---
+++ src/test/java/org/task/welcome.java
@@ -0,0 +1,12 @@
+package org.task;
+
+import org.testng.annotations.BeforeClass;
+
+public class welcome{
+	@BeforeClass
+	private void beforeclass() {
+		System.out.println("Before class");
+
+	}
+
+}
